d6089c1499d1b02353fcbe9ef51f057016c46b9f
Fabian Zimmer <fabian@qfma.de>
2016-07-20 15:10:21 +0100

    Added a screencast to add the maftools using a Json import

--- make-screencast-gif.sh
+++ make-screencast-gif.sh
@@ -1,1 +1,2 @@
 ffmpeg -i 2016-07-19-CGC-ICR-Workshop-screencast.mov -filter:v scale=1000:-1 -r 10 2016-07-19-CGC-ICR-Workshop-screencast.gif
+ffmpeg -i 2016-07-19-CGC-ICR-Workshop-screencast-addApp.mov -filter:v scale=1000:-1 -r 10 2016-07-19-CGC-ICR-Workshop-screencast-addApp.gif
